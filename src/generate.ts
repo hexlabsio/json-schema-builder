ae4900be703e125f156382bd8d777ac63a7e9930
Chris Barbour <chris.barbour@hexlabs.io>
2022-12-15 11:23:42 +0000

    improved naming

--- src/generate.ts
+++ src/generate.ts
@@ -7,7 +7,8 @@ import { Printer } from '@hexlabs/typescript-generator';
 (async () => {
   const result = await axios.get('http://json-schema.org/draft-07/schema#');
   const updated = { ...result.data, title: 'JSONSchema7' };
-  const builder = SchemaToTsBuilder.create(updated);
+  const builder = SchemaToTsBuilder.create(updated)
+    .transformNames(ref => ref.name === 'JSONSchema7' ? {...ref, name: 'JSONSchema7Type'} : ref.name === 'JSONSchema7Type_0' ? {...ref, name: 'JSONSchema7'} : ref);
   const schemaFile = builder.modelFile('json-schema.ts');
   const schemaBuilder = builder.builderFile();
   schemaBuilder.forEach(it => schemaFile.append(`export ${(it as any).print(Printer.create())}`));
