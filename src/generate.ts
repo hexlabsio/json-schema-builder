852ebab996d0fadde237d27a9c921046007c9b1a
Chris Barbour <chris.barbour@hexlabs.io>
2022-12-29 12:07:00 +0000

    improved naming

--- src/generate.ts
+++ src/generate.ts
@@ -4,8 +4,13 @@ import axios from 'axios';
 (async () => {
   const result = await axios.get('http://json-schema.org/draft-07/schema#');
   const updated = { ...result.data, title: 'JSONSchema7' };
-  const builder = SchemaToTsBuilder.create(updated, 'generated');
-    // .transformNames(ref => ref.name === 'JSONSchema7' ? {...ref, name: 'JSONSchema7Type'} : ref.name === 'JSONSchema7Type_0' ? {...ref, name: 'JSONSchema7'} : ref);
+  const builder = SchemaToTsBuilder.create(updated, 'generated', (name, location) => {
+    if(name === 'JSONSchema7Enum') return { name: 'Enumeration', location };
+    if(name === 'JSONSchema7_Type') return { name: 'Schema', location };
+    if(name === 'JSONSchema7Type_1') return { name: 'JSONSchema7Array', location };
+    if(name.startsWith('JSONSchema7') && name !== 'JSONSchema7') return { name: name.substring(11), location };
+    return { name, location };
+  });
   const schemaFile = builder.modelFiles();
   schemaFile.write()
 })();
--- src/json-schema.ts
+++ src/json-schema.ts
@@ -1,12 +1,12 @@
 import { JSONSchema7, JSONSchema7Builder } from 'generated/json-schema.org/draft-07/JSONSchema7';
-import { JSONSchema7_Type } from 'generated/json-schema.org/draft-07/JSONSchema7_Type';
+import { Schema } from 'generated/json-schema.org/draft-07/Schema';
 
 export class Schemas<K extends string | null = null> {
 
-  private constructor(private location: string, private schemas: Record<K extends null ? string : K, JSONSchema7_Type> = {} as any) {
+  private constructor(private location: string, private schemas: Record<K extends null ? string : K, Schema> = {} as any) {
   }
 
-  add<Name extends string>(name: Name, schema: JSONSchema7_Type | ((builder: S<K extends null ? Name : K | Name>) => JSONSchema7_Type)): Schemas<K extends null ? Name : K | Name> {
+  add<Name extends string>(name: Name, schema: Schema | ((builder: S<K extends null ? Name : K | Name>) => Schema)): Schemas<K extends null ? Name : K | Name> {
     if(typeof schema === 'function') {
       this.schemas[name as any] = schema(new S(this.location, name));
     } else {
@@ -25,38 +25,101 @@ export class Schemas<K extends string | null = null> {
   }
 }
 
+type CommonProps = Pick<JSONSchema7, 'title' | 'description' | 'const' | 'enum' | 'default' | 'readOnly' | 'writeOnly' | 'examples'>
+type StringProps = Pick<JSONSchema7, keyof CommonProps | 'format' | 'minLength' | 'maxLength' | 'pattern'>
+type ObjectProps = Pick<JSONSchema7, keyof CommonProps | 'additionalProperties' | 'properties' | 'required' | 'patternProperties' | 'minProperties' | 'maxProperties'>
+type ArrayProps = Pick<JSONSchema7, keyof CommonProps | 'additionalItems' | 'items' | 'minItems' | 'maxItems' | 'uniqueItems'>
+type NumberProps = Pick<JSONSchema7, keyof CommonProps | 'minimum' | 'maximum' | 'exclusiveMinimum' | 'exclusiveMaximum'>
+type IntegerProps = Pick<JSONSchema7, keyof CommonProps | 'minimum' | 'maximum' | 'exclusiveMinimum' | 'exclusiveMaximum'>
+type BooleanProps = Pick<JSONSchema7, keyof CommonProps> & { type: 'boolean' }
+type NullProps = Pick<JSONSchema7, keyof CommonProps> & { type: 'null' }
+type RefProps = Pick<JSONSchema7, '$ref'>
+
 export class S<K extends string = string> {
 
   constructor(private location: string, private name?: string) {
   }
 
-  ref(name: K extends null ? string : K): JSONSchema7_Type {
-    return { $ref: `${this.location}/${name}` };
+  ref(name: K extends null ? string : K): RefProps {
+    return S.$ref(`${this.location}/${name}`);
   }
 
-  string(props: JSONSchema7): JSONSchema7_Type {
+  static $ref($ref: string): RefProps {
+    return { $ref };
+  }
+
+  string(props?: StringProps): StringProps {
+    return S.string(props);
+  }
+
+  static string(props?: StringProps): JSONSchema7 {
     return { type: 'string', ...props };
   }
 
-  object(props: { title?: string; required?: Record<string, JSONSchema7_Type>, optional?: Record<string, JSONSchema7_Type>, additionalProperties?: JSONSchema7_Type, overrides?: JSONSchema7 }): JSONSchema7_Type {
-    return S.object({...props, title: props.title ?? this.name });
+  number(props?: NumberProps): JSONSchema7 {
+    return S.number(props);
   }
 
-  schema(schema: JSONSchema7_Type | ((builder: JSONSchema7Builder<{}>) => JSONSchema7Builder)): JSONSchema7_Type {
+  static number(props?: NumberProps): JSONSchema7 {
+    return { type: 'number', ...props };
+  }
+
+  integer(props?: IntegerProps): JSONSchema7 {
+    return S.integer(props);
+  }
+
+  static integer(props?: IntegerProps): JSONSchema7 {
+    return { type: 'integer', ...props };
+  }
+
+  boolean(props?: BooleanProps): JSONSchema7 {
+    return S.boolean(props);
+  }
+
+  static boolean(props?: BooleanProps): JSONSchema7 {
+    return { type: 'boolean', ...props };
+  }
+
+  null(props?: NullProps): JSONSchema7 {
+    return S.null(props);
+  }
+
+  static null(props?: NullProps): JSONSchema7 {
+    return { type: 'null', ...props };
+  }
+
+  arrayOf(reference: K extends null ? string : K, props?: ArrayProps): JSONSchema7 {
+    return S.array({ items: this.ref(reference), ...props });
+  }
+
+  array(props?: ArrayProps): JSONSchema7 {
+    return S.array(props);
+  }
+
+  static array(props?: ArrayProps): JSONSchema7 {
+    return { type: 'array', ...props };
+  }
+
+  object(props?: { title?: string; required?: Record<string, Schema>, optional?: Record<string, Schema> } & ObjectProps): JSONSchema7 {
+    return S.object(props ? {...props, title: props.title ?? this.name } : { title: this.name });
+  }
+
+  static object(props?: { title?: string; required?: Record<string, Schema>, optional?: Record<string, Schema> } & ObjectProps): JSONSchema7 {
+    if(!props) return { type: 'object' };
+    const {title, required, optional, ...rest} = props
+    return {
+      type: 'object',
+      ...(title ? { title: title } : {} ),
+      ...(required ? { required: Object.keys(required) } : {}),
+      properties: { ...required, ...optional },
+      ...rest
+    };
+  }
+
+  schema(schema: Schema | ((builder: JSONSchema7Builder<{}>) => JSONSchema7Builder)): Schema {
     if(typeof schema === 'function') {
       return schema(JSONSchema7Builder.create()).build();
     }
     return schema;
-  }
-
-  static object(props: { title?: string; required?: Record<string, JSONSchema7_Type>, optional?: Record<string, JSONSchema7_Type>, additionalProperties?: JSONSchema7_Type, overrides?: JSONSchema7 }): JSONSchema7_Type {
-    return {
-      type: 'object',
-      ...(props.title ? { title: props.title } : {} ),
-      ...(props.required ? { required: Object.keys(props.required) } : {}),
-      properties: { ...props.required, ...props.optional },
-      ...(props.additionalProperties != undefined ? { additionalProperties: props.additionalProperties } : {}),
-      ...props.overrides
-    };
   }
 }
